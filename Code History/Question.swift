f852feb9f851596bb942804dcca9517c93e22bc8
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-09-24 15:03:03 +0100

    struct for asking Questions

---
+++ Code History/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Reuben Kouidri on 24/09/2023.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
