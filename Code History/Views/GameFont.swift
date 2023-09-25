e2094ce1d10b1913a21e383b54bac5330969e310
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-09-25 14:04:28 +0100

    refactored colours into struct

---
+++ Code History/Views/GameFont.swift
@@ -0,0 +1,16 @@
+//
+//  GameFonts.swift
+//  Code History
+//
+//  Created by Reuben Kouidri on 25/09/2023.
+//
+
+import SwiftUI
+
+
+struct GameFont {
+    static let buttonFont = Font.custom("Helvetica", size: 16)
+    static let questionFont = Font.largeTitle
+    static let welcomeFont = Font.largeTitle
+    static let smallFont = Font.callout
+}
